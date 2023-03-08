0323b8e8215f275580aa2bee032ad819e2c00b13
BCD15 <bruno69correadias@gmail.com>
2023-03-08 15:07:30 -0300

    nada demais

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,33 @@
 import { Component } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
+import { TextInput } from "react-native-web";
 
 export default class Counter extends Component {
   state = {
     count: 0,
+    step: 2,
   };
   increment() {
-    this.setState({ count: this.state.count + 100000000000000000 });
+    this.setState({ count: this.state.count + this.state.step });
   }
   diminuiment() {
-    this.setState({ count: this.state.count - 30000000000 });
+    this.setState({ count: this.state.count - this.state.step });
+  }
+  alterarStep(texto) {
+    this.setState({step: parseInt(texto) || 0});
   }
   render() {
     return (
       <View style={styles.contador}>
+        <TextInput
+        v alue={this.state.step.toString()}
+        onChange={(e) => this.alterarStep(e.nativeEvent.text)}
+        keyboardType="numeric"
+        />
         <Text>
           Contador: {this.state.count}
-          <Button onPress={() => this.increment()} title="adicinar numero pra kct" />
-          <Button onPress={() => this.diminuiment()} title="perde numeros buaaaaa(ian gay)" />
+          <Button onPress={() => this.increment()} title="+" />
+          <Button onPress={() => this.diminuiment()} title="-" />
         </Text>
       </View>
     );
@@ -30,4 +40,5 @@ const styles = StyleSheet.create({
     marginButton: 10,
     alignItems: "center",
   },
+
 });
